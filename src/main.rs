843069d7d727453afb994f346acc8399a2c2d101
Minwoo Jeong <jeongmwj@gmail.com>
2022-11-14 19:42:19 +0900

    Get the user's number from STDIN

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use rand::Rng;
-
+use std::io::{self, Write};
 fn main() {
     let mut rng = rand::thread_rng();
     let secret_number = rng.gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
+
+    print!("Enter your number: ");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Invalid input");
+
+    let number: u32 = input.trim().parse().expect("Enter a valid number!");
+    println!("Your input number: {number}");
 }
