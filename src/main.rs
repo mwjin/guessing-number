2d1209c642326409d23bca6d55a2aea54fb08968
Minwoo Jeong <jeongmwj@gmail.com>
2022-11-14 19:47:08 +0900

    Compare the guessing number with the secret number

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use rand::Rng;
-use std::io::{self, Write};
+use std::{
+    cmp::Ordering,
+    io::{self, Write},
+};
+
 fn main() {
     let mut rng = rand::thread_rng();
     let secret_number = rng.gen_range(1..=100);
@@ -12,6 +16,11 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Invalid input");
 
-    let number: u32 = input.trim().parse().expect("Enter a valid number!");
-    println!("Your input number: {number}");
+    let guess: u32 = input.trim().parse().expect("Enter a valid number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small!"),
+        Ordering::Greater => println!("Too Big!"),
+        Ordering::Equal => println!("You win!"),
+    };
 }
